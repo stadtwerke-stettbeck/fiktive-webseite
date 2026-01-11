5d7fde45b26428de8077c835a7f7384d5dac3439
stadtwerke-stettbeck <stadtwerke.stettbeck@gmail.com>
2026-01-11 18:34:01 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showWelcomeAlert() {
-  var x = document.getElementById("nav_id");
-  if (x.className === "nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav";
-  }
-}
